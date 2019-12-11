7a041b8ce6da4b81ec89ace087be5db901e35793
Cucumberbob <cucumberdoesminecraft@gmail.com>
2019-12-11 17:30:52 +0000

    Added finding and replacement of vertical lines

--- Board.py
+++ Board.py
@@ -2,21 +2,89 @@ class Board:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        
+
         self._board = []
         for _ in range(y):
             self._board.append(['-' for i in range(x)])
 
-    def __str__(self):
+    def place(self, x, y, piece):
+        if x > self.x or y > self.y or x <= 0 or y <= 0:
+            raise PlacementError("invalid coordinates")
+
+        self._board[self.y - y][x - 1] = piece
+        self.update(x, y, piece)
+
+    def update(self, x, y, piece):
+        # TODO modularization and skip if multiple of same color in a row
+        # NB: this doesn't really affect functionality, but it makes more sense
+        distance = 0
+        direction = ""
+
+        # Check upwards
+        for i in range(y, self.y + 1):
+            if self.y - i == self.y - y:
+                continue
+
+            if self._board[self.y - i][x-1] == piece:
+                newDistance = abs(i - y) - 1
+                if newDistance > distance:
+                    foundX = x - 1
+                    foundY = i - y
+                    distance = newDistance
+                    direction = "u"
+
+        # Check downwards
+        for i in range(y, 0, -1):
+            if self.y - i == self.y - y:
+                continue
+
+            if self._board[self.y - i][x-1] == piece:
+                newDistance = abs(i - y) - 1
+                if newDistance > distance:
+                    foundX = x - 1
+                    foundY = y - i
+                    distance = newDistance
+                    direction = "d"
+
+        if distance == 0:
+            return False
+
+        print(f"{distance=}")
+
+        print(f"{foundX=} {foundY=} {direction=}")
+        self.replace(x, y, distance, direction, piece)
+
+    def replace(self, x, y, distance, direction, piece):
+        if direction == 'u':
+            for i in range(y + 1, y + 1 + distance):
+                self._board[self.y-i][x - 1] = piece
+            return
+
+        if direction == 'd':
+            for i in range(y - distance, y):
+                self ._board[self.y-i][x - 1] = piece
+            return
+
+    def toString(self):
         stringified = ""
 
-        for _ in range(self.x):
-            stringified += '--'
-        stringified += '\n'
+        line = ""
+        for _ in range(3 + (self.x*2)):
+            line += '-'
+        line += '\n'
+
+        stringified += line
+
         for y in range(self.y):
             stringified += '| '
             for x in range(self.x):
                 stringified += self._board[y][x] + ' '
             stringified += '|\n'
 
+        stringified += line
+
         return stringified
+
+
+class PlacementError(Exception):
+    pass
--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 from Board import Board
 
-b = Board(4,4)
 
-print(b)
+size = 6
+b = Board(size, size)
+
+w = 'w'
+bl = 'b'
+
+b.place(1, size, w)
+print(b.toString())
+
+for i in range(2, size):
+    print(i)
+    b.place(1, i, bl)
+
+print(b.toString())
+
+b.place(1, 1, w)
+
+print(b.toString())
+
+# b.place(1, 5, w)
+
+# print(b.toString())
